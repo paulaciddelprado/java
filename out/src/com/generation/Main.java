8aca737aecf908cb5cd76c7226bc6662b714a89a
paulaciddelprado <90283054+paulaciddelprado@users.noreply.github.com>
2021-10-11 17:27:56 -0500

    Add files via upload

---
+++ out/src/com/generation/Main.java
@@ -0,0 +1,11 @@
+package com.generation;
+
+public class Main {
+
+    public static void main(String[] args) {
+        const edad = 28;
+        String nombre = "Paula Cid del Prado";
+        System.out.println (nombre);
+	// write your code here
+    }
+}
