a2fa8fd23035ff7f707acae167f1bdc66f041e3f
paulaciddelprado <90283054+paulaciddelprado@users.noreply.github.com>
2021-10-20 17:50:29 -0500

    Add files via upload

--- poo-avanzado/src/com/generation/paquete2/Main.java
+++ poo-avanzado/src/com/generation/paquete2/Main.java
@@ -1,15 +1,40 @@
 package com.generation.paquete2;
 
 import com.generation.banco.Person;
+//Los atributos van depsués de la clase
 
 public class Main {
+    public static int numerador = 10;
+    public static int denominador = 0;
+    public static float division;
 
     public static void main(String[] args) {
 
-                Person person= new Person("Paula", 28, "10023456");
-                /*System.out.println( "Name: " + person.name);
+
+            System.out.println("Antes de hacer la división");
+
+            try {
+                division = numerador/denominador;
+            } catch (NullPointerException ex) {
+                System.out.println("NullPointer");
+                System.out.println(ex.getMessage());
+            } catch (Exception ex) {
+                ex.getMessage();
+                System.out.println("Exception");
+                System.out.println(ex.getMessage());
+            } finally  {
+                System.out.println("Soy finally");
+            }
+
+            System.out.println("Después de hacer la división");
+        }
+    }
+
+
+               /* Person person= new Person("Paula", 28, "10023456");
+                System.out.println( "Name: " + person.name);
                 System.out.println( "Age: "+ person.age);
-                System.out.println( "Id: " +  person.id);*/
+                System.out.println( "Id: " +  person.id);
 
                 person.setName("Paula");
                 person.setAge(28);
@@ -17,10 +42,10 @@ public class Main {
 
         System.out.println(person.getName());
         System.out.println(person.getAge());
-        System.out.println(person.getId());
+        System.out.println(person.getId()); */
 
-            }
-        }
+
+
 
         /*CuentaBancaria cuenta = new CuentaBancaria();
         cuenta.setNombre("Paula Cid del Prado");
